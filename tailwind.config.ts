8d1c91dd728d141045a784c5462d4497ba7ad116
Sharanjeet Kaur <sharanjeetkaur@Sharanjeets-MacBook-Pro.local>
2025-07-14 21:51:21 -0700

    fixing tailwind file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,28 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: ["class"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-theme": "#f3f5f9",
+        "dark-theme": "bg-gray-950",
+      },
+      animation: {
+        wobble: "wobble 1.5s ease-in-out infinite",
+      },
+      keyframes: {
+        wobble: {
+          "0%, 100%": { transform: "rotate(-1deg)" },
+          "50%": { transform: "rotate(1deg)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+} satisfies Config;
