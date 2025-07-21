27c2b94bd0647524eb8dfb2149dc306531effc3a
Sharanjeet Kaur <sharanjeetkaur@Sharanjeets-MacBook-Pro.local>
2025-07-20 17:06:58 -0700

    added all the suggested changes

--- src/app/components/SearchBar/index.tsx
+++ src/app/components/SearchBar/index.tsx
@@ -1,40 +1,29 @@
 "use client";
 
 import { useOutsideClick } from "@/app/hooks/useClickOutside";
-import { ChangeEvent, useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { Coin } from "@/app/interfaces/Coininterface";
+import useDebounce from "@/app/hooks/useDebounce";
 
 export default function SearchBar() {
   const [data, setData] = useState<Coin[]>([]);
   const [query, setQuery] = useState("");
   const [isOpen, setIsOpen] = useState(true);
-
   const ref = useRef(null);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
 
-  //timer as ueref is not updated on every rerender
-  const timer = useRef<HTMLDivElement>(null);
-  function debounce(callback: (...args: any[]) => void, delay: number) {
-    let timeout: NodeJS.Timeout;
-    return (...args: any[]) => {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        callback(...args);
-      }, delay);
-    };
-  }
   const fetchData = async () => {
     const response = await fetch(
       `https://api.coingecko.com/api/v3/search?key=CG-YHe92rLkyEoghZERKMWNmW5K&query=${query.toLowerCase()}`
     );
     const allData = await response.json();
-
     setData(allData.coins);
   };
-  const debouncedFetchData = debounce(fetchData, 100);
+  //	debouncedFetchData-- debounced version of fetchData, and you use this instead of calling fetchData directly.
+  const debouncedFetchData = useDebounce(fetchData, 100);
 
   const closeList = () => {
     setIsOpen(false);
@@ -49,8 +38,6 @@ export default function SearchBar() {
   useEffect(() => {
     if (query) {
       debouncedFetchData();
-    } else {
-      setData([]); // Clear results if query is empty
     }
   }, [query]);
 
