edf9261894d27b0c804a609094926a718385b996
Shaeranw01 <sharan.w01@gmail.com>
2025-09-09 17:00:25 -0700

    minor changes in carousel

---
+++ src/app/components/CarouselCoinContainer/index.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+import PriceChange from "../PriceChange";
+
+export default function CarouselCoinContainer({
+  selected,
+  image,
+  name,
+  symbol,
+  current_price,
+  price_change_24h,
+  selectedCurrency,
+}: {
+  selected: boolean;
+  image: string;
+  name: string;
+  symbol: string;
+  current_price: number;
+  price_change_24h: number;
+  selectedCurrency: string;
+}) {
+  return (
+    <div
+      className={`${
+        selected
+          ? " bg-[#6161D680] text-[#424286] dark:bg-[#6161D680] dark:text-white"
+          : "bg-white dark:bg-[#232336] text-[#424286]  dark:text-white"
+      } w-[250px] h-[80px] flex items-center  rounded-lg p-2`}
+    >
+      <Image src={image} width={32} height={32} alt="Picture of the coin" />
+
+      <div className="flex flex-col gap-1">
+        <div className="dark:text-white text-[#353570] ">
+          {name}
+          <span>({symbol.toUpperCase()})</span>
+        </div>
+        <div className="flex items-center gap-1">
+          <div>
+            {current_price?.toFixed(2)} {selectedCurrency?.toUpperCase()}
+          </div>
+          <PriceChange value={price_change_24h} />
+        </div>
+      </div>
+    </div>
+  );
+}
